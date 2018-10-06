1eafb579d8f27a390c149283e09263696d5903c5
CourtneyDL <courtneyleacock@googlemail.com>
2018-10-06 11:04:03 +0100

    TaskScheduler.getSchedule

--- test/lib/TaskScheduler.spec.js
+++ test/lib/TaskScheduler.spec.js
@@ -48,7 +48,43 @@ describe('TaskScheduler.getSchedule', () => {
         const dependencies_in = [];
         const result = [];
 
-        const task_scheduler = new TaskScheduler();
-        expect(task_scheduler.getSchedule()).to.deep.equal([]);
+        const task_scheduler = new TaskScheduler(tasks_in, dependencies_in);
+        expect(task_scheduler.getSchedule()).to.deep.equal(result);
+    });
+
+    it('should return a schedule matching the tasks provided if no dependencies are provided', () => {
+        const tasks_in = ['a','b'];
+        const dependencies_in = [];
+        const result = ['a','b'];
+
+        const task_scheduler = new TaskScheduler(tasks_in, dependencies_in);
+        expect(task_scheduler.getSchedule()).to.deep.equal(result);
+    });
+
+    it('should return the correct schedule for 2 tasks and 1 dependency', () => {
+        const tasks_in = ['a','b'];
+        const dependencies_in = ['a => b'];
+        const result = ['b','a'];
+
+        const task_scheduler = new TaskScheduler(tasks_in, dependencies_in);
+        expect(task_scheduler.getSchedule()).to.deep.equal(result);
+    });
+
+    it('should return the correct schedule for 4 tasks and 2 dependencies', () => {
+        const tasks_in = ['a','b','c','d'];
+        const dependencies_in = ['a => b','c => d'];
+        const result = ['b','a','d','c'];
+
+        const task_scheduler = new TaskScheduler(tasks_in, dependencies_in);
+        expect(task_scheduler.getSchedule()).to.deep.equal(result);
+    });
+
+    it('should return the correct schedule for 3 tasks and 2 dependencies', () => {
+        const tasks_in = ['a','b','c'];
+        const dependencies_in = ['a => b','b => c'];
+        const result = ['c','b','a'];
+
+        const task_scheduler = new TaskScheduler(tasks_in, dependencies_in);
+        expect(task_scheduler.getSchedule()).to.deep.equal(result);
     });
 });
